ee49044497f21a45bd3809ebeb7b190b411a54cd
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-04-28 14:32:52 -0500

    Login y Recuperacion
    Terminados, de nada

--- resources/views/auth/BORRARtest.blade.php
+++ resources/views/auth/BORRARtest.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
@@ -26,27 +26,51 @@
 
 <body>
     <div class="row">
-        <div class="divleft col s12 m7 l5">
-            <div class="col s12 head-login">
+        <div class="divright  hide-on-small-only col s12 m3 l4">
+            <div class="row" style="width: 100%">
+                <div class="col s12 m12 l12" style="display: block">
+                    <ul class="tabs">
+                        <li class="tab col xl6 l6 m6"><a href="#test1">Ingresar</a></li>
+                        <li class="tab col xl6 l6 m6"><a class="" href="#test2">Registrarse</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col s12 head-login forgot left-align">
                 <div class="left">
                     <div class="titulo1">Escuela Sindical</div>
                     <div class="titulo2">Sección XX Michoacán</div>
                 </div>
                 <div class="right">
-                    <img src="{{ asset('img/imagenes/LogoSeccional.png') }}" alt="Escuela Sindical" width="121px">
+                    <img src="{{ asset('img/imagenes/LogoSeccional.png') }}" alt="Escuela Sindical" width="80px">
                 </div>
             </div>
-
+            <div class="col s12">
+                <img src="{{ asset('img/imagenes/forgot-password-cuate.svg') }}" alt="" srcset="">
+            </div>
+        </div>
+        <div class="divleft col s12 m9 l8">
+            <div class="col s12 head-login">
+                <div class="left">
+                    <div class="titulo2">RECUPERAR CONTRASEÑA</div>
+                </div>
+            </div>
+            @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+            @endif
             <div class="form-login col s12">
-                <h1>INICIAR SESIÓN</h1>
-                <h2>Bienvenido de nuevo</h2>
-                <form class="col s12" method="POST" action="{{ route('login') }}">
+                <h1>RECUPERAR CONTRASEÑA</h1>
+                <h2 style="font-size: 16px">Ingresa tu nueva contraseña</h2>
+                <form class="col s12" method="POST" action="{{ route('password.update') }}">
                     @csrf
+                    <input type="hidden" name="token" value="{{ $token }}">
                     <div class="row">
                         <div class="input-field col s12" style="margin-top: 6.5vh; padding:0">
                             <i class="material-icons prefix">face</i>
                             <input id="matricula" type="email" class="validate @error('email') invalid @enderror"
-                                id="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                id="email" name="email" value="{{ $email ?? old('email') }}" required
+                                autocomplete="email">
                             <label for="matricula">Correo electrónico</label>
                             @error('email')
                             <span class="helper-text" data-error="{{$message}}">{{$message}}</span>
@@ -55,7 +79,7 @@
                         <div class="input-field col s12" style="margin-top: 6.5vh;padding:0">
                             <i class="material-icons prefix">lock</i>
                             <input id="password" type="password" class="validate" name="password" required
-                                autocomplete="current-password">
+                                autocomplete="new-password">
                             <label for="password">Contraseña</label>
                         </div>
                         @error('password')
@@ -63,12 +87,18 @@
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
+                        <div class="input-field col s12" style="margin-top: 6.5vh;padding:0">
+                            <i class="material-icons prefix">lock</i>
+                            <input id="password-confirm" type="password" class="validate" name="password_confirmation"
+                                required autocomplete="new-password">
+                            <label for="password-confirm">Confirmar Contraseña</label>
+                        </div>
                     </div>
-                    <a class="link-forget" href="{{ route('password.request') }}">¿Olvidaste tu contraseña?</a>
+
                     <div class="col s12 right-align">
                         <button class="btn waves-effect waves-teal btn-login" type="submit" name="action"
-                            style="height: 54px">Ingresar
-                            <i class="material-icons right">login</i>
+                            style="height: 54px">Cambiar contraseña
+                            <i class="material-icons right">send</i>
                         </button>
                     </div>
                 </form>
@@ -78,20 +108,6 @@
                 © 2021 Escuela Sindical | &nbsp; <a href="https://dragonware.com.mx" target="_blank"> Desarrollado por
                     DragonWare
                     <img src="{{ asset('img/imagenes/dragonWare.png') }}" alt="DragonWare" width="22px"></a>
-            </div>
-        </div>
-
-        <div class="divright  hide-on-small-only col s12 m5 l7">
-            <div class="row" style="width: 100%">
-                <div class="col s12 m12 l10" style="display: block">
-                    <ul class="tabs">
-                        <li class="tab col xl3 l4 m6"><a href="#test1">Ingresar</a></li>
-                        <li class="tab col xl3 l4 m6"><a class="active" href="#test2">Registrarse</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col s12">
-                <img src="{{ asset('img/imagenes/Onlinelearning-amico.svg') }}" alt="" srcset="">
             </div>
         </div>
     </div>
