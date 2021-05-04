a55394f54e43ef86bbe56a470f23c73095eb58a1
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-05-03 21:24:23 -0500

    Ya hice mis primeros 2 componentes

--- resources/js/Pages/Usuarios/Usuarios.js
+++ resources/js/Pages/Usuarios/Usuarios.js
@@ -1,46 +1,50 @@
 import React from 'react';
+import FlotanteAyuda from '../../components/common/FlotanteAyuda';
+import Paginacion from '../../components/common/Paginacion';
 import Layout from '../../layouts/Layout';
 
-const Usuarios = ({users}) => {
-  return (
-    <>
-        <div className="row">
-            <div className="col">
-                <div className="card darken-1">
-                    <div className="card-content">
-                        <span className="card-title">Usuarios</span>
-                        <table className="striped">
-                            <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                            </tr>
-                            </thead>
+const Usuarios = ({ users }) => {
+    return (
+        <>
+            <div className="row">
+                <div className="col">
+                    <div className="card darken-1">
+                        <div className="card-content">
+                            <span className="card-title">Usuarios</span>
+                            <table className="striped">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                    </tr>
+                                </thead>
 
-                            <tbody>
-                            { users.map(user => (
-                                <tr key={user.id}>
-                                    <td>{user.nombre}</td>
-                                    <td>{user.email}</td>
-                                    {/*<td>
+                                <tbody>
+                                    {users.map(user => (
+                                        <tr key={user.id}>
+                                            <td>{user.nombre}</td>
+                                            <td>{user.email}</td>
+                                            {/*<td>
                                         <InertiaLink href={`/users/${user.id}/edit`}>Edit</InertiaLink>
                                     </td>*/}
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="card-action">
-                        <a href="#">This is a link</a>
-                        <a href="#">This is a link</a>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="card-action">
+                            <a href="#">This is a link</a>
+                            <a href="#">This is a link</a>
+                        </div>
+                        <Paginacion />
                     </div>
                 </div>
             </div>
-        </div>
-    </>
-  )
+            <FlotanteAyuda />
+        </>
+    )
 }
 
-Usuarios.layout = page => <Layout children={page} title="Escuela Sindical - Usuarios" pageTitle="USUARIOS"/>
+Usuarios.layout = page => <Layout children={page} title="Escuela Sindical - Usuarios" pageTitle="USUARIOS" />
 
 export default Usuarios
