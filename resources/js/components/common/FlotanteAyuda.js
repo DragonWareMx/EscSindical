33c4182f5970ab54fb9fe9ca7437cf1708c91aeb
PapaDrago <oscarwaii@hotmail.com>
2021-05-04 00:47:18 -0500

    modalsito chiquito
    chiquitito

--- resources/js/components/common/FlotanteAyuda.js
+++ resources/js/components/common/FlotanteAyuda.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom'
 //import circlesImg from '../images/circles.png'
 //import emptyImg from '../images/empty.png'
 //import './styles/Card.css'
+import '/css/modal.css'
 
 
 function initializeModals() {
@@ -19,14 +20,40 @@ export default function FlotanteAyuda() {
             <div className="fixed-action-btn">
                 <a className="btn-floating btn-large waves-effect waves-light green-sind modal-trigger" href="#modalHelp"><i class="material-icons">help_outline</i></a>
             </div>
-
-            <div id="modalHelp" className="modal">
+            <div id="modalHelp" className="modal little-modal">
                 <div className="modal-content">
-                    <h4>Modal Header</h4>
-                    <p>A bunch of text</p>
-                </div>
-                <div className="modal-footer">
-                    <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
+                    <div className="modal-close right"><i className="material-icons">close</i></div>
+                    <div className="row">
+                        <div className="col s12 center-align">
+                            <img src="img/imagenes/ActiveSupport-amico1.png"></img>
+                        </div>
+                        <div className="col s12 center-align modal-title">¿Necesitas ayuda? Contáctanos</div>
+                        <div className="col s12 modal-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod </div>
+                        <div className="row" style={{"margin":"0px"}}>
+                            <div className="col s1">
+                                <i class="material-icons" style={{"color":"#656565","margin-top":"12px"}}>language</i>
+                            </div>
+                            <div className="col s11">
+                                <div className="" style={{"color":"#5C5C5C","font-size":"15px","margin-top":"12px","padding-left":"10px"}}>www.sindicatoxx.com</div>
+                            </div>
+                        </div>
+                        <div className="row" style={{"margin":"0px"}}>
+                            <div className="col s1">
+                                <i class="material-icons" style={{"color":"#656565","margin-top":"12px"}}>mail_outline</i>
+                            </div>
+                            <div className="col s11">
+                                <div className="" style={{"color":"#5C5C5C","font-size":"15px","margin-top":"12px","padding-left":"10px"}}>escuelasindical@imss.com</div>
+                            </div>
+                        </div>
+                        <div className="row" style={{"margin":"0px"}}>
+                            <div className="col s1">
+                                <i class="material-icons" style={{"color":"#656565","margin-top":"12px"}}>call</i>
+                            </div>
+                            <div className="col s11">
+                                <div className="" style={{"color":"#5C5C5C","font-size":"15px","margin-top":"12px","padding-left":"10px"}}>44 44 44 44 44</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
