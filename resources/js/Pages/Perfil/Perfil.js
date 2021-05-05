d4e23561f4d195b4d28e5c1d22d2485ad95a1e31
AdolfoLem <74695413+AdolfoLem@users.noreply.github.com>
2021-05-05 01:28:36 -0500

    front de ver perfil y medio back

--- resources/js/Pages/Perfil/Perfil.js
+++ resources/js/Pages/Perfil/Perfil.js
@@ -5,66 +5,115 @@ import Paginacion from '../../components/common/Paginacion';
 import FlotanteAyuda from '../../components/common/FlotanteAyuda';
 
 import '../../styles/profileStyle.css'
+import ProfilePic from '../../../../public/img/monita.jpg'
 
-const Perfil = ({ }) => {
-
+const Perfil = ({ user }) => {
 
     return (
         <>
-            <div className="row orange">
-                <div className="col s3">
+            <div className="row">
+                <div className="col s12 m4">
                     <div className="row">
-                        <div className="col s12">
+                        <div className="col s12" style={{"marginTop":"30px"}}>
                             <div className="card">
                                 <div className="card-content">
-                                    Aqui va el nombre
+                                    <div className="row">
+                                        <div className="col s12 center-align">
+                                            <div className="center-align">
+                                                <img src={ProfilePic} alt="foto de perfil" className="img-profile"/>
+                                            </div>
+                                        </div>
+                                        <div className="col s12 center-align">
+                                            <div className="profile-txt-category">
+                                                Categoría
+                                            </div>
+                                            <div className="profile-txt-name">
+                                                {user.nombre} {user.apellido_p} {user.apellido_m}  
+                                            </div>
+                                            <div className="profile-txt-active-since">
+                                                Activo desde {user.created_at}
+                                            </div>
+                                            <div className="profile-txt-email truncate">
+                                                    <i class="material-icons profile-icon-email">mail_outline</i>
+                                                    {user.email}
+                                            </div>
+                                            {/* Boton de enviar mensaje */}
+                                            <div className="col s12" style={{"marginTop":"15px"}}>
+                                                <a class="waves-effect waves-light btn boton-verde"><i class="material-icons right" style={{"font-size":"18px"}}>send</i>Enviar Mensaje</a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col s9 purple">
+                <div className="col s12 m8">
                     <div className="row">
                         <div className="col s12">
                             <div className="card">
                                 <div className="card-content">
-                                    <div>
+                                    <div className="title-section">
                                         INFORMACIÓN PERSONAL
                                     </div>
-                                    <div>
+                                    <div className="info-section">
                                         Tu información personal no será visible para el público.
                                     </div>
                                     <div className="row">
-                                        <div className="col s6 red">
-                                            INFORMACIÓN PERSONAL
-                                            <div>
-                                                Fecha de Nacimiento: 
+                                        <div className="col s12 m6" style={{"marginTop":"15px"}}>
+                                            <div className="titles-sub">
+                                                INFORMACIÓN PERSONAL
                                             </div>
-                                            <div>
-                                                Sexo:
+                                            <div className="info-txt-format">
+                                                Fecha de Nacimiento: {user.fecha_nac}
+                                            </div>
+                                            <div className="info-txt-format">
+                                                Sexo: {user.sexo}
                                             </div>
                                         </div>
-                                        <div className="col s6 blue">
-                                            INFORMACIÓN INSTITUCIONAL
-                                            <div>
-                                                Matrícula:
+                                        <div className="col s12 m6" style={{"marginTop":"15px"}}>
+                                            <div className="titles-sub">
+                                                INFORMACIÓN INSTITUCIONAL
                                             </div>
-                                            <div>
-                                                Regimen:
+                                            <div className="info-txt-format">
+                                                Matrícula: {user.matricula}
                                             </div>
-                                            <div>
+                                            <div className="info-txt-format">
+                                                Regimen: 
+                                            </div>
+                                            <div className="info-txt-format">
                                                 Unidad: 
                                             </div>
-                                            <div>
-                                                Categoría:
+                                            <div className="info-txt-format">
+                                                Categoría: {user.categorie_id}
                                             </div>
                                         </div>
-                                        <div className="col s6 teal">
-                                            DIRECCIÓN
+                                        <div className="col s12 m6" style={{"marginTop":"15px"}}>
+                                            <div className="titles-sub">
+                                                DIRECCIÓN
+                                            </div>
+                                            <div className="info-txt-format">
+                                                Estado: {user.estado}
+                                            </div>
+                                            <div className="info-txt-format">
+                                                Ciudad: {user.ciudad}
+                                            </div>
+                                            <div className="info-txt-format">
+                                                Colonia: {user.colonia}
+                                            </div>
+                                            <div className="info-txt-format">
+                                                Calle: {user.calle}
+                                            </div>
+                                            <div className="info-txt-format">
+                                                No. Exterior: {user.num_ext}
+                                            </div>
+                                            <div className="info-txt-format">
+                                                Código Postal: {user.cp}
+                                            </div>
                                         </div>
-                                        
-                                        <div className="col s6 pink">
-                                            Aquí va el botón
+                                        {/* Boton de editar */}
+                                        <div className="col s12 m6 right-align" style={{"marginTop":"15px"}}>
+                                            <a class="waves-effect waves-light btn boton-verde"><i class="material-icons right" style={{"font-size":"18px"}}>edit</i>Editar</a>
                                         </div>
                                     </div>
                                 </div>
