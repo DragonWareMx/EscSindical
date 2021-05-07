b7ffc05302932a9190163d3e60bc6b46ddc83247
AdolfoLem <74695413+AdolfoLem@users.noreply.github.com>
2021-05-07 00:19:27 -0500

    margin bottom al footer

--- resources/js/components/layout/Footer.js
+++ resources/js/components/layout/Footer.js
@@ -7,7 +7,7 @@ return(
     <div>
         {/* footer de la pagina */}
         <div className="footer-copyright center-align">
-            <div style={{"color":"#707070"}}>
+            <div style={{"color":"#707070"},{"marginBottom":"20px"}}>
                 © 2021 Escuela Sindical   |   Desarrollado por DragonWare <img src={Logo} alt="logo"/>
             </div>
             
