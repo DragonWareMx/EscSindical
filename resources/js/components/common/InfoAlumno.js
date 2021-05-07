604ecd63c671ca36c6905e3c912fac5bad3e7455
PapaDrago <oscarwaii@hotmail.com>
2021-05-07 01:21:13 -0500

    no me dejan cenar temprano

--- resources/js/components/common/InfoAlumno.js
+++ resources/js/components/common/InfoAlumno.js
@@ -71,13 +71,15 @@ export default function InfoAlumno({user}) {
         day = '' + d.getDate(),
         year = d.getFullYear();
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
 
-    return [year, month, day].join('-');
+        return [year, month, day].join('-');
     }
+
+
 
 
     const [state, setState] = useState({
