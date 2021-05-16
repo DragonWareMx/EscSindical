e0bade75736a5438efefe244424ffaa66dba3a59
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-05-16 18:13:55 -0500

    Nuevo hover de tarjetas
    Si no les gusta se deshace este commit :v

--- resources/js/components/cursos/CourseCardSearch.js
+++ resources/js/components/cursos/CourseCardSearch.js
@@ -7,36 +7,60 @@ import '/css/courseCardSearch.css'
 
 export default function CourseCardSearch({ curso }) {
 
+    function transformaFecha(fecha) {
+        const dob = new Date(fecha);
+        const monthNames = [
+            'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+            'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+        ];
+        const day = dob.getDate();
+        const monthIndex = dob.getMonth();
+        const year = dob.getFullYear();
+        return `${day} ${monthNames[monthIndex]} ${year}`;
+    }
+
     return (
         <div className="col s12">
-            <div className="card">
+            <div className="card course-card">
                 {/* Imagen del curso */}
-                <div className="col s12 course-image-container center-align">
+                <div className="col s12 course-image-container center-align transicion">
                     {/* <img src="/images/monita.jpg" alt="imagen del curso" className="course-image" /> */}
-                    <img className="course-image" src={curso.first_image.length > 0 ? '/storage/imagenes_curso/' + curso.first_image[0].imagen : '/storage/imagenes_curso/default.png'} alt="img" />
+                    <img className="course-image transicion" src={curso.first_image.length > 0 ? '/storage/imagenes_curso/' + curso.first_image[0].imagen : '/storage/imagenes_curso/default.png'} alt="img" />
                 </div>
                 {/* Informacion sobre el curso */}
                 <div className="card-content">
-                    <div className="row">
+                    <div className="row" style={{ "marginBottom": "0px" }}>
                         {/* Nombre del curso */}
-                        <div className="col s12 valign-wrapper" style={{"marginTop":"7px"}}>
+                        <div className="col s12 valign-wrapper transicion course-name" style={{ "marginTop": "7px" }}>
                             <i class="material-icons verified-icon">verified</i>
-                            <span className="course-name truncate">{curso.nombre}</span>
+                            {curso.nombre}
                         </div>
                         {/* Nombre del ponente */}
-                        <div className="col s12" style={{"marginTop":"5px"}}>
+                        <div className="col s12" style={{ "marginTop": "5px" }}>
                             <span className="course-teacher truncate">{curso.teacher.nombre} {curso.teacher.apellido_p} {curso.teacher.apellido_m}</span>
                         </div>
                         {/* Tags del curso */}
-                        <div className="col s12 courseCard_tags" style={{"marginTop":"5px"}}>
+                        <div className="col s12 courseCard_tags" style={{ "marginTop": "5px" }, { "marginBottom": "5px" }}>
                             <div className="container-tags">
-                                {curso.tags.map(tag=>
+                                {curso.tags.map(tag =>
                                     <a className="div-tag" href="#!">
                                         {tag.nombre} <i class="material-icons" style={{ "fontSize": "12px" }}>local_offer</i>
                                     </a>
                                 )}
                             </div>
                         </div>
+                        <div className="fechas-card transicion">
+                            <h3>Inscripciones</h3>
+                            <div>{transformaFecha(curso.inicio_inscripciones)}-{transformaFecha(curso.fecha_limite)} </div>
+                            <h3>Fechas del curso</h3>
+                            <div>{transformaFecha(curso.fecha_inicio)}-{transformaFecha(curso.fecha_final)} </div>
+                        </div>
+                        <div className="button-course col s12 transicion" >
+                            <button class="btn waves-effect waves-light" name="action">Inscribirme
+                                <i class="material-icons right">add_circle</i>
+                            </button>
+                        </div>
+
                     </div>
                 </div>
             </div>
