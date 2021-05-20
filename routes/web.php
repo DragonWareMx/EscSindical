1c5f5a92562b6728b1426678ceb34d47573d49e1
Dulce Gabriela Marín Rendón <dgmarin61@gmail.com>
2021-05-20 10:16:12 -0500

    me faltaba recibir un id

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::get('/cursos/edit/{id}', [App\Http\Controllers\CourseController::class, '
 Route::post('/storeCourse', [App\Http\Controllers\CourseController::class, 'store'])->name('storeCourse');
 // ------Modulos del curso------
 Route::get('/cursos/module/create', [App\Http\Controllers\CourseController::class, 'moduleCreate'])->name('module.create');
-Route::get('/cursos/module/edit', [App\Http\Controllers\CourseController::class, 'moduleEdit'])->name('module.edit');
+Route::get('/cursos/module/edit/{id}', [App\Http\Controllers\CourseController::class, 'moduleEdit'])->name('module.edit');
 
 //------Rutas del layout cursos
 Route::get('/cursos/{id}/informacion', [App\Http\Controllers\CourseController::class, 'informacion'])->name('cursos.informacion');
