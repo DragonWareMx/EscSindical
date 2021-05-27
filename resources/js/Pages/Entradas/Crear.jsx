e6d79ddb0e17084c46585856979c0499524593b6
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-05-26 23:29:02 -0500

    Avance en la vista de crear entrada
    Faltan algunos detalles :v

--- resources/js/Pages/Entradas/Crear.jsx
+++ resources/js/Pages/Entradas/Crear.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { InertiaLink, usePage } from '@inertiajs/inertia-react'
 import Layout from '../../layouts/Layout';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
@@ -22,28 +23,13 @@ const Crear = () => {
 
     //valores para formulario
     const [values, setValues] = useState({
-        nombre: "",
-        apellido_paterno: "",
-        apellido_materno: "",
-        email: "",
-        contrasena: "",
-        confirmar_contrasena: "",
-        fecha_de_nacimiento: "",
-        sexo: "",
-        matricula: "",
-        categoria: "",
-        unidad: "",
-        regimen: "",
-        estado: "",
-        ciudad: "",
-        colonia: "",
-        calle: "",
-        codigo_postal: "",
-        numero_exterior: "",
-        numero_interior: "",
-        tarjeton_de_pago: "",
-        foto: null,
-        rol: ""
+        tipo: "",
+        titulo: "",
+        contenido: "",
+        visible: "",
+        notificacion: "",
+        link: "",
+        link_del_examen: ""
     })
 
     //actualiza los hooks cada vez que se modifica un input
@@ -56,6 +42,97 @@ const Crear = () => {
         }))
     }
 
+    function ocultarTodos() {
+        var input = document.getElementById("link-div");
+        input.style.display = "none";
+        var input = document.getElementById("link-examen-div");
+        input.style.display = "none";
+        var input = document.getElementById("ckeditor-div");
+        input.style.display = "none";
+        var input = document.getElementById("file-div");
+        input.style.display = "none";
+        var input = document.getElementById("fecha-a-div");
+        input.style.display = "none";
+        var input = document.getElementById("fecha-e-div");
+        input.style.display = "none";
+        var input = document.getElementById("switch-envios-div");
+        input.style.display = "none";
+    }
+
+    function mostrarAviso() {
+        ocultarTodos();
+        var input = document.getElementById("ckeditor-div");
+        input.style.display = "block";
+        var input = document.getElementById("file-div");
+        input.style.display = "block";
+    }
+
+    function mostrarEnlace() {
+        ocultarTodos();
+        var input = document.getElementById("link-div");
+        input.style.display = "block";
+    }
+
+    function mostrarArchivo() {
+        ocultarTodos();
+        var input = document.getElementById("file-div");
+        input.style.display = "block";
+    }
+
+    function mostrarAsignacion() {
+        ocultarTodos();
+        var input = document.getElementById("ckeditor-div");
+        input.style.display = "block";
+        var input = document.getElementById("file-div");
+        input.style.display = "block";
+        var input = document.getElementById("fecha-a-div");
+        input.style.display = "block";
+        var input = document.getElementById("fecha-e-div");
+        input.style.display = "block";
+        var input = document.getElementById("switch-envios-div");
+        input.style.display = "block";
+    }
+
+    function mostrarExamen() {
+        ocultarTodos();
+        var input = document.getElementById("link-examen-div");
+        input.style.display = "block";
+        var input = document.getElementById("ckeditor-div");
+        input.style.display = "block";
+        var input = document.getElementById("fecha-a-div");
+        input.style.display = "block";
+        var input = document.getElementById("fecha-e-div");
+        input.style.display = "block";
+        var input = document.getElementById("switch-envios-div");
+        input.style.display = "block";
+    }
+
+    function cambiarForm() {
+        var tipo = document.getElementById("tipo").value;
+        switch (tipo) {
+            case "Aviso":
+                mostrarAviso();
+                break;
+            case "Informacion":
+                mostrarAviso();
+                break;
+            case "Enlace":
+                mostrarEnlace();
+                break;
+            case "Archivo":
+                mostrarArchivo();
+                break;
+            case "Asignacion":
+                mostrarAsignacion();
+                break;
+            case "Examen":
+                mostrarExamen();
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <>
             <div className="container">
@@ -63,29 +140,39 @@ const Crear = () => {
                     <div className="col s12">
                         <div className="card">
                             <div className="card-content card-entradas">
-                                <span class="card-title">AGREGAR ENTRADA</span>
+                                <span className="card-title">AGREGAR ENTRADA</span>
                                 <form action="">
                                     <div className="row">
                                         {/* aqui va el input del select tipo */}
                                         <div className="input-field col s12 m6">
-                                            <select>
-                                                <option value disabled selected>Elige una opción</option>
-                                                <option value={Aviso}>Aviso</option>
-                                                <option value={Informacion}>Información</option>
-                                                <option value={Enlace}>Enlace (Link)</option>
-                                                <option value={Archivo}>Archivo</option>
-                                                <option value={Asignacion}>Asignación</option>
-                                                <option value={Examen}>Examen</option>
+                                            <select id="tipo" name="tipo" value={values.tipo} onChange={handleChange, cambiarForm}>
+                                                <option value disabled value={""}>Elige una opción</option>
+                                                <option value={"Aviso"}>Aviso</option>
+                                                <option value={"Informacion"}>Información</option>
+                                                <option value={"Enlace"}>Enlace (Link)</option>
+                                                <option value={"Archivo"}>Archivo</option>
+                                                <option value={"Asignacion"}>Asignación</option>
+                                                <option value={"Examen"}>Examen</option>
                                             </select>
                                             <label>Tipo de recurso</label>
                                         </div>
                                         {/* Aqui va el input del titulo */}
                                         <div className="input-field col s12 m6">
-                                            <input id="titulo" name="titulo" type="text" className="validate" />
+                                            <input id="titulo" name="titulo" type="text" className="validate" value={values.titulo} onChange={handleChange} />
                                             <label htmlFor="titulo">Título de la entrada</label>
                                         </div>
+                                        {/* Aqui va el input del link */}
+                                        <div id="link-div" className="input-field col s12" style={{ display: "none" }}>
+                                            <input id="link" name="link" type="text" className="validate" value={values.link} onChange={handleChange} />
+                                            <label htmlFor="link">Link de la entrada</label>
+                                        </div>
+                                        {/* Aqui va el input del link del examen */}
+                                        <div id="link-examen-div" className="input-field col s12" style={{ display: "none" }}>
+                                            <input id="link_del_examen" name="link_del_examen" type="text" className="validate" value={values.link} onChange={handleChange} />
+                                            <label htmlFor="link">Link del examen</label>
+                                        </div>
                                         {/* Aqui va el CKeditor */}
-                                        <div className="col s12">
+                                        <div id="ckeditor-div" className="col s12" style={{ display: "none" }}>
                                             <h2 className="ck-titulo">Contenido</h2>
                                             <CKEditor
                                                 editor={ClassicEditor}
@@ -107,7 +194,7 @@ const Crear = () => {
                                             />
                                         </div>
                                         {/* -- INPUT FILE -- */}
-                                        <div className="col s12" style={{ marginBottom: "2%", marginTop: "2%" }}>
+                                        <div id="file-div" className="col s12" style={{ marginBottom: "2%", marginTop: "2%", display: "none" }}>
                                             <p style={{ "marginTop": "0px", "fontFamily": "Montserrat", "fontSize": "13px", color: "rgb(159, 157, 157)" }}>Adjuntar archivos (Opcional)</p>
                                             <div className="file-field input-field" style={{ "border": "1px dashed rgba(159, 157, 157, 0.6)", boxSizing: "border-box", borderRadius: "4px" }}>
                                                 <div className="col s12">
@@ -119,8 +206,16 @@ const Crear = () => {
                                                 </div>
                                             </div>
                                         </div>
+                                        {/* fecha de apertura */}
+                                        <div id="fecha-a-div" className="col s12 m6" style={{ display: "none" }}>
+                                            aqui va el input
+                                        </div>
+                                        {/* fecha de entrega */}
+                                        <div id="fecha-e-div" className="col s12 m6" style={{ display: "none" }}>
+                                            aqui va el input
+                                        </div>
                                         {/* primer switch */}
-                                        <div className="col s12 m4 l3">
+                                        <div className="col s12 m4 l3" style={{ display: "none" }}>
                                             <h2 className="ck-titulo">Visible dentro del curso</h2>
                                             <div className="switch">
                                                 <label>
@@ -132,8 +227,20 @@ const Crear = () => {
                                             </div>
                                         </div>
                                         {/* segundo switch */}
-                                        <div className="col s12 m4 l3">
+                                        <div className="col s12 m4 l3" style={{ display: "none" }}>
                                             <h2 className="ck-titulo">Notificar a los participantes</h2>
+                                            <div className="switch">
+                                                <label>
+                                                    No
+                                                <input type="checkbox" />
+                                                    <span className="lever" />
+                                                    Si
+                                            </label>
+                                            </div>
+                                        </div>
+                                        {/* tercer switch envios retrasados */}
+                                        <div id="switch-envios-div" className="col s12 m4 l3" style={{ display: "none" }}>
+                                            <h2 className="ck-titulo">Permitir envios retrasados</h2>
                                             <div className="switch">
                                                 <label>
                                                     No
