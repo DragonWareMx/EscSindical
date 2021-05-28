8f4a38c885ed9e1c4460fdc9d0699426ff79f006
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-05-27 21:42:13 -0500

    Ya acabé el front de Crear Entrada bien como se debe
    De nada

--- database/migrations/2021_04_25_021445_create_entries_table.php
+++ database/migrations/2021_04_25_021445_create_entries_table.php
@@ -16,15 +16,17 @@ class CreateEntriesTable extends Migration
         Schema::create('entries', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('titulo',45);
-            $table->enum('tipo', ['enum1','enum2']);
-            $table->string('contenido',45);
-            $table->unsignedInteger('max_calif');
+            $table->string('titulo', 255);
+            $table->enum('tipo', ['Aviso', 'Informacion', 'Enlace', 'Archivo', 'Asignacion', 'Examen']);
+            $table->text('contenido');
             $table->unsignedBigInteger('module_id');
             $table->dateTime('fecha_act');
             $table->dateTime('fecha_limite');
             $table->tinyInteger('visible');
-            $table->tinyInteger('editable');
+            $table->tinyInteger('permitir_envios_retrasados');
+            $table->text('link');
+            $table->dateTime('fecha_de_apertura');
+            $table->dateTime('fecha_de_entrega');
 
             $table->foreign('module_id')->references('id')->on('modules')->onDelete('cascade');
 
