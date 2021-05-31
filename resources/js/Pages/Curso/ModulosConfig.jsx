70e78c8420f2362cf9631fd12414376a5e183507
Dulce Gabriela Marín Rendón <dgmarin61@gmail.com>
2021-05-30 21:20:02 -0500

    EDITAR MODULOS

--- resources/js/Pages/Curso/ModulosConfig.jsx
+++ resources/js/Pages/Curso/ModulosConfig.jsx
@@ -44,7 +44,7 @@ const ModulosConfig = ({curso}) => {
     <>
         <div className="row">
             <div className="col s12 m9 l10 xl10 titulo-modulo left" style={{marginTop:"15px"}}>MÓDULOS</div>
-            
+            <p className="col s12 text-ins-module">Es posible reordenar los módulos arrastrandolos, este orden será visible para todos los participantes dentro del curso</p>
             
             
             
@@ -52,10 +52,19 @@ const ModulosConfig = ({curso}) => {
                 <li>
                     <div className="col s12 div-modulo-config">
                         <div className="col s1">
-                            A
+                            <i className="material-icons" style={{"cursor":"move"}}>drag_handle</i>
                         </div>
                         <div className="col s11">
-                            <div className="col s12 txt-titulo-modulo-card">1. Nombre completo del módulo</div>
+                            <div className="div-info-modulo-c">
+                                <div className="col s12 m11 l11 xl11 txt-titulo-modulo-card">Nombre completo del módulo A</div>
+                                <div className="col s12 m1 l1 xl1"><a  className="dropdown-trigger right" data-target='dropdown-option-module'><i className="material-icons" style={{"color":"#727272", "fontSize":"22px"}}>more_vert</i></a></div>
+                                {/* Dropdown modulos */}
+                                <ul id='dropdown-option-module' className='dropdown-content dropdown_LC'>
+                                    <li><a className="dropdown-text modal-trigger" href="#!"><i className="material-icons">edit</i>Editar módulo</a></li>
+                                    <li><a className="dropdown-text modal-trigger" href="#!"><i className="material-icons">clear</i>Eliminar módulo</a></li>
+                                </ul>
+
+                            </div>
                             <div className="col s12 txt-titulo-objetivo">OBJETIVO</div>
                             <div className="col s12 txt-objetivo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.</div>
                         </div>
@@ -64,10 +73,19 @@ const ModulosConfig = ({curso}) => {
                 <li>
                     <div className="col s12 div-modulo-config">
                         <div className="col s1">
-                            B
+                            <i className="material-icons" style={{"cursor":"move"}}>drag_handle</i>
                         </div>
                         <div className="col s11">
-                            <div className="col s12 txt-titulo-modulo-card">1. Nombre completo del módulo</div>
+                            <div className="div-info-modulo-c">
+                                <div className="col s12 m11 l11 xl11 txt-titulo-modulo-card">Nombre completo del módulo B</div>
+                                <div className="col s12 m1 l1 xl1"><a  className="dropdown-trigger right" data-target='dropdown-option-module'><i className="material-icons" style={{"color":"#727272", "fontSize":"22px"}}>more_vert</i></a></div>
+                                {/* Dropdown modulos */}
+                                <ul id='dropdown-option-module' className='dropdown-content dropdown_LC'>
+                                    <li><a className="dropdown-text modal-trigger" href="#!"><i className="material-icons">edit</i>Editar módulo</a></li>
+                                    <li><a className="dropdown-text modal-trigger" href="#!"><i className="material-icons">clear</i>Eliminar módulo</a></li>
+                                </ul>
+
+                            </div>
                             <div className="col s12 txt-titulo-objetivo">OBJETIVO</div>
                             <div className="col s12 txt-objetivo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.</div>
                         </div>
@@ -76,10 +94,19 @@ const ModulosConfig = ({curso}) => {
                 <li>
                     <div className="col s12 div-modulo-config">
                         <div className="col s1">
-                            C
+                            <i className="material-icons" style={{"cursor":"move"}}>drag_handle</i>
                         </div>
                         <div className="col s11">
-                            <div className="col s12 txt-titulo-modulo-card">1. Nombre completo del módulo</div>
+                            <div className="div-info-modulo-c">
+                                <div className="col s12 m11 l11 xl11 txt-titulo-modulo-card">Nombre completo del módulo C</div>
+                                <div className="col s12 m1 l1 xl1"><a  className="dropdown-trigger right" data-target='dropdown-option-module'><i className="material-icons" style={{"color":"#727272", "fontSize":"22px"}}>more_vert</i></a></div>
+                                {/* Dropdown modulos */}
+                                <ul id='dropdown-option-module' className='dropdown-content dropdown_LC'>
+                                    <li><a className="dropdown-text modal-trigger" href="#!"><i className="material-icons">edit</i>Editar módulo</a></li>
+                                    <li><a className="dropdown-text modal-trigger" href="#!"><i className="material-icons">clear</i>Eliminar módulo</a></li>
+                                </ul>
+
+                            </div>
                             <div className="col s12 txt-titulo-objetivo">OBJETIVO</div>
                             <div className="col s12 txt-objetivo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.</div>
                         </div>
