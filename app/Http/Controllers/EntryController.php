adef8ca50aba64537af68ede6e8604511e0d4fb3
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-05-31 13:38:22 -0500

    Falta validar las fechas
    Ya casi :'v

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -250,7 +250,7 @@ class EntryController extends Controller
                 'modulo' => 'required|numeric|exists:modules,id',
                 'titulo' =>  ['required', 'max:255'],
                 'contenido' => 'required',
-                'archivos' => 'nullable|file',
+                'archivos.*' => 'nullable|file',
                 'visible' => 'required|boolean',
                 'notificacion' => 'required|boolean',
                 'permitir_envios_retrasados' => 'required|boolean',
@@ -258,9 +258,57 @@ class EntryController extends Controller
                 'fecha_de_entrega' => 'required|date',
                 'hora_de_apertura' => 'required|date_format:H:i',
                 'hora_de_entrega' => 'required|date_format:H:i',
+                'max_calif' => 'required|numeric',
             ]);
 
             //comprobar curso y modulo
+            $curso = Course::with('modules')->where([
+                ['teacher_id', Auth::user()->id,],
+                ['id', $request->curso]
+            ])->first();
+            if (!$curso) {
+                return Redirect::back()->with('error', 'Ha ocurrido un error al intentar crear la entrada, inténtelo más tarde.');
+            }
+            $modulo = Module::where([
+                ['id', $request->modulo],
+                ['course_id', $curso->id]
+            ])->first();
+            if (!$modulo) {
+                return Redirect::back()->with('error', 'Ha ocurrido un error al intentar crear la entrada, inténtelo más tarde.');
+            }
+            //aqui empieza la transaccion
+            DB::beginTransaction();
+            try {
+                $entrada = new Entry();
+                $entrada->titulo = $request->titulo;
+                $entrada->tipo = $request->tipo;
+                $entrada->max_calif = $request->max_calif;
+                $entrada->contenido = $request->contenido;
+                $entrada->module_id = $request->modulo;
+                $entrada->fecha_de_apertura = $request->fecha_de_apertura . ' ' . $request->hora_de_apertura;
+                $entrada->fecha_de_entrega = $request->fecha_de_entrega . ' ' . $request->hora_de_entrega;
+                $entrada->visible = $request->visible;
+                $entrada->permitir_envios_retrasados = $request->permitir_envios_retrasados;
+                $entrada->save();
+                //aqui va lo de los archivos
+                if ($request->file('archivos')) {
+                    foreach ($request->file('archivos') as $file) {
+                        $archivo = $file->store('public/archivos_cursos');
+                        $name = $file->hashName();
+                        $newFile = new File();
+                        $newFile->archivo = $name;
+                        $newFile->entry_id = $entrada->id;
+                        $newFile->save();
+                    }
+                }
+                DB::commit();
+                // all good
+                return Redirect::back()->with('success', 'La entrada se ha creado con éxito!');
+            } catch (\Exception $e) {
+                DB::rollback();
+                // something went wrong
+                return Redirect::back()->with('error', 'Ha ocurrido un error al intentar crear la entrada, inténtelo más tarde.');
+            }
         }
         if ($tipo == "Examen") {
             $validated = $request->validate([
@@ -276,9 +324,48 @@ class EntryController extends Controller
                 'fecha_de_entrega' => 'required|date',
                 'hora_de_apertura' => 'required|date_format:H:i',
                 'hora_de_entrega' => 'required|date_format:H:i',
+                'max_calif' => 'required|numeric',
             ]);
 
             //comprobar curso y modulo
+            $curso = Course::with('modules')->where([
+                ['teacher_id', Auth::user()->id,],
+                ['id', $request->curso]
+            ])->first();
+            if (!$curso) {
+                return Redirect::back()->with('error', 'Ha ocurrido un error al intentar crear la entrada, inténtelo más tarde.');
+            }
+            $modulo = Module::where([
+                ['id', $request->modulo],
+                ['course_id', $curso->id]
+            ])->first();
+            if (!$modulo) {
+                return Redirect::back()->with('error', 'Ha ocurrido un error al intentar crear la entrada, inténtelo más tarde.');
+            }
+            //aqui empieza la transaccion
+            DB::beginTransaction();
+            try {
+                $entrada = new Entry();
+                $entrada->titulo = $request->titulo;
+                $entrada->tipo = $request->tipo;
+                $entrada->link = $request->link;
+                $entrada->max_calif = $request->max_calif;
+                $entrada->contenido = $request->contenido;
+                $entrada->module_id = $request->modulo;
+                $entrada->fecha_de_apertura = $request->fecha_de_apertura . ' ' . $request->hora_de_apertura;
+                $entrada->fecha_de_entrega = $request->fecha_de_entrega . ' ' . $request->hora_de_entrega;
+                $entrada->visible = $request->visible;
+                $entrada->permitir_envios_retrasados = $request->permitir_envios_retrasados;
+                $entrada->save();
+
+                DB::commit();
+                // all good
+                return Redirect::back()->with('success', 'La entrada se ha creado con éxito!');
+            } catch (\Exception $e) {
+                DB::rollback();
+                // something went wrong
+                return Redirect::back()->with('error', 'Ha ocurrido un error al intentar crear la entrada, inténtelo más tarde.');
+            }
         }
         dd($request);
     }
