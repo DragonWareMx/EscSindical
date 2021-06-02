b345abf360b1c60c131c9ed58ead4c8c522c7500
AdolfoLem <74695413+AdolfoLem@users.noreply.github.com>
2021-06-01 22:21:14 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/Oski-el-chungo' into AdolFofas""
    This reverts commit e1ee7d194d1276ae503390009dc8841b0a8f3108.

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::get('/cursos', [App\Http\Controllers\CourseController::class, 'index'])->
 Route::get('/cursos/create', [App\Http\Controllers\CourseController::class, 'create'])->name('cursos.create');
 Route::get('/cursos/edit/{id}', [App\Http\Controllers\CourseController::class, 'editCourse'])->name('cursos.edit');
 Route::post('/storeCourse', [App\Http\Controllers\CourseController::class, 'store'])->name('storeCourse');
-
+Route::post('/updateCourse/{id}', [App\Http\Controllers\CourseController::class, 'update'])->name('updateCourse');
 
 // ------Modulos del curso------
 Route::get('/cursos/module/create', [App\Http\Controllers\CourseController::class, 'moduleCreate'])->name('module.create');
@@ -59,10 +59,17 @@ Route::post('/cursos/module/store', [App\Http\Controllers\CourseController::clas
 
 //------Rutas del layout cursos
 Route::get('/cursos/{id}/informacion', [App\Http\Controllers\CourseController::class, 'informacion'])->name('cursos.informacion');
+
 Route::get('/cursos/{id}/modulos', [App\Http\Controllers\CourseController::class, 'modulos'])->name('cursos.modulos');
 Route::get('/cursos/{id}/modulo/{mid}', [App\Http\Controllers\CourseController::class, 'modulo'])->name('cursos.modulo');
+
+
+
+
+
 Route::get('/cursos/{id}/participantes', [App\Http\Controllers\CourseController::class, 'participantes'])->name('cursos.participantes');
 Route::get('/cursos/{id}/solicitudes', [App\Http\Controllers\CourseController::class, 'solicitudes'])->name('cursos.solicitudes');
+Route::get('/cursos/{id}/agregar-participante', [App\Http\Controllers\CourseController::class, 'agregarParticipante'])->name('cursos.agregarParticipante');
 Route::get('/cursos/{id}/mochila', [App\Http\Controllers\CourseController::class, 'mochila'])->name('cursos.mochila');
 Route::get('/cursos/{id}/modulo/{mid}/publicacion/{pid}', [App\Http\Controllers\CourseController::class, 'verPublicacion'])->name('cursos.publicacion');
 
