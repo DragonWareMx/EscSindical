e31819335e63008197b3160156ff04b2f6094a2e
AdolfoLem <74695413+AdolfoLem@users.noreply.github.com>
2021-06-13 23:24:54 -0500

    Update ModulosConfig.jsx

--- resources/js/Pages/Curso/ModulosConfig.jsx
+++ resources/js/Pages/Curso/ModulosConfig.jsx
@@ -33,25 +33,25 @@ const ModulosConfig = ({curso}) => {
             {curso.modules['0'] ? 
                 <ul id="items" className="col s12">
                     {curso.modules.map((modulo) =>
-                    <li key={modulo.id}>
-                        <div className="col s12 div-modulo-config">
-                            <div className="col s12">
-                                <div className="div-info-modulo-c">
-                                    <InertiaLink href={route('cursos.modulo',[curso.id,modulo.id])} className="col s12 m11 l11 xl11 txt-titulo-modulo-card">{modulo.nombre}</InertiaLink>
-                                    <div className="col s12 m1 l1 xl1"><a  className="dropdown-trigger right" data-target={'dropdown-option-module'+modulo.id}><i className="material-icons" style={{"color":"#727272", "fontSize":"22px"}}>more_vert</i></a></div>
-                                    {/* Dropdown modulos */}
-                                    <ul id={'dropdown-option-module'+modulo.id} className='dropdown-content dropdown_LC'>
-                                        <li><a className="dropdown-text modal-trigger" href={route('module.edit',modulo.id)}><i className="material-icons">edit</i>Editar módulo</a></li>
-                                        <li><a className="dropdown-text modal-trigger" data-target={'modalEliminar'+modulo.nombre} href=""><i className="material-icons">clear</i>Eliminar módulo</a></li>
-                                    </ul>
-                                
+                        <li key={modulo.id}>
+                            <div className="col s12 div-modulo-config">
+                                <div className="col s12">
+                                    <div className="div-info-modulo-c">
+                                        <InertiaLink href={route('cursos.modulo',[curso.id,modulo.id])} className="col s12 m11 l11 xl11 txt-titulo-modulo-card">{modulo.nombre}</InertiaLink>
+                                        <div className="col s12 m1 l1 xl1"><a  className="dropdown-trigger right" data-target={'dropdown-option-module'+modulo.id}><i className="material-icons" style={{"color":"#727272", "fontSize":"22px"}}>more_vert</i></a></div>
+                                        {/* Dropdown modulos */}
+                                        <ul id={'dropdown-option-module'+modulo.id} className='dropdown-content dropdown_LC'>
+                                            <li><a className="dropdown-text modal-trigger" href={route('module.edit',modulo.id)}><i className="material-icons">edit</i>Editar módulo</a></li>
+                                            <li><a className="dropdown-text modal-trigger" data-target={'modalEliminar'+modulo.nombre} href=""><i className="material-icons">clear</i>Eliminar módulo</a></li>
+                                        </ul>
+                                    
+                                    </div>
+                                    <div className="col s12 txt-titulo-objetivo">OBJETIVO</div>
+                                    <div className="col s12 txt-objetivo">{modulo.objetivo}</div>
                                 </div>
-                                <div className="col s12 txt-titulo-objetivo">OBJETIVO</div>
-                                <div className="col s12 txt-objetivo">{modulo.objetivo}</div>
                             </div>
-                        </div>
-                        <ModalEliminar nombre={modulo.nombre} tipo={'modulo'} url = {route('module.delete', modulo.id)}/>
-                    </li>
+                            <ModalEliminar nombre={modulo.nombre} tipo={'modulo'} url = {route('module.delete', modulo.id)}/>
+                        </li>
                     )}
                 </ul>
             :
