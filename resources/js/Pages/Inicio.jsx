714340ba05687fb6f03511089ce66450947a4be8
Dulce Gabriela Marín Rendón <dgmarin61@gmail.com>
2021-06-30 00:50:00 -0500

    detalles detalles

--- resources/js/Pages/Inicio.jsx
+++ resources/js/Pages/Inicio.jsx
@@ -16,7 +16,7 @@ const Home = () => {
                           <div className="col s12 m6">
                             <div className="col s12 txt-welcome-prov">¡BIENVENIDO DE NUEVO!</div>
                             <div className="col s12 txt-sub-prov">Explorá los cursos que Escuela Sindical tiene para ti</div>
-                            <div className="col s12 txt-prov">Encuentra el curso adecuado para ti y unete a la clase, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor. </div>
+                            <div className="col s12 txt-prov">Encuentra el curso adecuado para ti y unete a la clase. <br></br>Nuestras conquistas se traducen a un sin fin de prestaciones económicas y sociales para los Trabajadores del IMSS y sus familias. </div>
                             <div className="col s12" style={{"marginTop":"50px","marginBottom":"20px"}}><InertiaLink href={route('cursosBuscar')} className="btn-search-prov">Buscar cursos</InertiaLink></div>
                           </div>
                           <div className="col s12 m6">
