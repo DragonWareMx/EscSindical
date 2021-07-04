ef2a658fb367209cffaaf4c9074ef054c3d7cddf
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-07-04 00:44:28 -0500

    Traduje los mensajes de error
    Ya

--- resources/js/Pages/Error.jsx
+++ resources/js/Pages/Error.jsx
@@ -5,17 +5,17 @@ import { InertiaLink } from '@inertiajs/inertia-react';
 
 function ErrorPage({ status }) {
     const title = {
-        503: '503: Service Unavailable',
-        500: '500: Server Error',
-        404: '404: Page Not Found',
-        403: '403: Forbidden',
+        503: '503: Servicio no disponible',
+        500: '500: Server interno',
+        404: '404: Página no encontrada',
+        403: '403: Prohibido',
     }[status]
 
     const description = {
-        503: 'Sorry, we are doing some maintenance. Please check back soon.',
-        500: 'Whoops, something went wrong on our servers.',
-        404: 'Sorry, the page you are looking for could not be found.',
-        403: 'Sorry, you are forbidden from accessing this page.',
+        503: 'Lo sentimos, estamos haciendo mantenimiento. Por favor vuelve luego.',
+        500: 'Whoops, algo salió mal en nuestros servidores.',
+        404: 'Lo sentimos, la página que estás buscando no se pudo encontrar.',
+        403: 'Lo sentimos, no tienes permisos para entrar a esta página.',
     }[status]
 
     return (
